4d9055f238cab54683d6681197e768bf55f0c585
adityakukreja <adityakukreja07@gmail.com>
2021-02-26 00:38:00 +0530

    final

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,19 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  emptyLogin : boolean = false;
-  emptySignUp : boolean = false;
+  
 
   constructor() { }
 
   ngOnInit(): void {
-    if(localStorage.getItem("loginUsername") == null){
-      this.emptyLogin = true;
-    }
-
-    if(localStorage.getItem("SignUpUsername") == null){
-      this.emptySignUp = true;
-    }
+    
   }
 
 }
